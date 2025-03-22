329e21af4c94770cf1daef45c7d0c28060bc3725
Mustafa Alperen <mustafaalperenpolat@gmail.com>
2025-03-22 16:46:33 +0300

    Add descripion

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,21 +1,22 @@
 ﻿
-void BubbleSort<T>(T[] array){
-    for (int i = 0; i < array.Length; i++)
-    {
-        for (int j = 0; j < array.Length-i-1; j++) //sonu hep sıralı gittiği için, gidilmiş sondaki elemanları çıkarıyoruz
-        {
-            if (Comparer<T>.Default.Compare(array[j],array[j+1])>0)//jninci eleman j+1'nci elemandan büyükse
-            {
-                var temp = array[j];
-                array[j]=array[j+1];
-                array[j+1]=temp;
-            }
-        }
-    }
+void BubbleSort<T>(T[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		for (int j = 0; j < array.Length - i - 1; j++) //sonu hep sıralı gittiği için, gidilmiş sondaki elemanları çıkarıyoruz en büyük elemanı her zaman en sona götürüyor
+		{
+			if (Comparer<T>.Default.Compare(array[j], array[j + 1]) > 0)//jninci eleman sağındaki elemandan büyükse
+			{
+				var temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
 
 
-int [] numbers = {5,2,7,9,1};
-string [] names = {"Ali","Veli","Ayşe","Fatma"};
+int[] numbers = { 5, 2, 7, 9, 1 };
+string[] names = { "Ali", "Veli", "Ayşe", "Fatma" };
 BubbleSort(numbers);
-System.Console.WriteLine(string.Join(',',numbers));
+System.Console.WriteLine(string.Join(',', numbers));
