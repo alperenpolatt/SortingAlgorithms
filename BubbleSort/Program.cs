f2deaddc75bfd2ceb08a2dd2e3eee2d15f064498
Mustafa Alperen <mustafaalperenpolat@gmail.com>
2025-03-23 19:02:02 +0300

    Add test

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 void BubbleSort<T>(T[] array)
 {
-	for (int i = 0; i < array.Length; i++)
+	for (int i = 0; i < array.Length; i++) // Dizinin her bir elemanı için yapıldığından
 	{
-		for (int j = 0; j < array.Length - i - 1; j++) //sonu hep sıralı gittiği için, gidilmiş sondaki elemanları çıkarıyoruz en büyük elemanı her zaman en sona götürüyor
+		for (int j = 0; j < array.Length - i - 1; j++) //sonu hep sıralı gittiği için, gidilmiş sondaki elemanları çıkarıyoruz 
 		{
-			if (Comparer<T>.Default.Compare(array[j], array[j + 1]) > 0)//jninci eleman sağındaki elemandan büyükse sağa taşıyacak
+			if (Comparer<T>.Default.Compare(array[j], array[j + 1]) > 0)//jninci eleman sağındaki elemandan büyükse sağa taşıyacak en büyük elemanı her zaman en sona götürüyor
 			{
 				var temp = array[j];
 				array[j] = array[j + 1];
---
+++ Test/Program.cs
@@ -0,0 +1,40 @@
+﻿void BubbleSort(int [] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		for (int j = 0; j < arr.Length-i-1; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+}
+
+void SelectionSort(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		var minIndex = i;
+		for (int j = i+1; j < arr.Length; j++)
+		{
+			if (arr[j] < arr[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+		swap(arr, i, minIndex);
+	}
+}
+
+void swap (int[] arr, int firstIndex, int secondIndex)
+{
+	int temp = arr[firstIndex];
+	arr[firstIndex] = arr[secondIndex];
+	arr[secondIndex] = temp;
+}
+
+int[] numbers = { 1, 2, 5, 3, 0 };
+BubbleSort(numbers);
+Console.WriteLine(string.Join(",",numbers));
