6fc0ad034c7ce2e2d33a272e53822e6983748563
alperenpolatt <mustafaalperenpolat@gmail.com>
2025-03-25 16:30:21 +0300

    Test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,40 +1,44 @@
-﻿void BubbleSort(int [] arr)
-{
-	for (int i = 0; i < arr.Length; i++)
-	{
-		for (int j = 0; j < arr.Length-i-1; j++)
-		{
-			if (arr[j] > arr[j+1])
-			{
-				swap(arr, j, j + 1);
-			}
-		}
-	}
-}
+﻿
+        
+		
+		string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-void SelectionSort(int[] arr)
-{
-	for (int i = 0; i < arr.Length; i++)
-	{
-		var minIndex = i;
-		for (int j = i+1; j < arr.Length; j++)
-		{
-			if (arr[j] < arr[minIndex])
-			{
-				minIndex = j;
-			}
-		}
-		swap(arr, i, minIndex);
-	}
-}
+        int n = Convert.ToInt32(firstMultipleInput[0]);
 
-void swap (int[] arr, int firstIndex, int secondIndex)
-{
-	int temp = arr[firstIndex];
-	arr[firstIndex] = arr[secondIndex];
-	arr[secondIndex] = temp;
-}
+        int k = Convert.ToInt32(firstMultipleInput[1]);
 
-int[] numbers = { 1, 2, 5, 3, 0 };
-BubbleSort(numbers);
-Console.WriteLine(string.Join(",",numbers));
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.pairs(k, arr);
+
+        Console.WriteLine(result);
+
+
+class Result
+{
+
+    /*
+     * Complete the 'pairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int pairs(int k, List<int> arr)
+    {
+        var total =0;
+        for (int i = 0; i < arr.Count-1; i++)
+        {
+            for (int j = i+1; j < arr.Count; j++)
+            {
+                var dif = Math.Abs(arr[i]-arr[j]);
+                if(dif==k)
+                    total++;
+            } 
+        }
+        return total;
+    }
+
+}
